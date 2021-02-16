12cdd12164ad2cfc33ea1bb1513cc916ebe47b2d
shaw <csfrancis@wisc.edu>
2021-02-16 14:38:39 -0600

    get rid of print statements

--- pyxem/utils/correlation_utils.py
+++ pyxem/utils/correlation_utils.py
@@ -78,7 +78,8 @@ def _correlation(z, axis=0, mask=None, wrap=True, normalize_axes=None):
         row_mean1 = np.expand_dims(row_mean1, axis=normalize_axes)
         row_mean2 = np.expand_dims(row_mean2, axis=normalize_axes)
         a = np.divide(np.subtract(a, row_mean1), row_mean2)
-        a[number_unmasked == 1] = 0
+        a[number_unmasked <5 ] = 0
+
 
     if wrap is False:
         a = a[slicer]
