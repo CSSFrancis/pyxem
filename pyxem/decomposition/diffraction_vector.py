ba98e474f4532ac723b6a32c2acc304c94d33d01
shaw <csfrancis@wisc.edu>
2021-12-10 14:16:56 -0600

    Fixed get_vdf function to slice the right area of the image.

--- pyxem/decomposition/diffraction_vector.py
+++ pyxem/decomposition/diffraction_vector.py
@@ -10,9 +10,10 @@ def get_vdf(data,
             vector,
             threshold=None,
             radius=2,
+            search=5,
             extent=None, ):
     shape = tuple(reversed(data.axes_manager.signal_shape))
-    rr, cc = disk(center=(vector[-2], vector[-3]),
+    rr, cc = disk(center=(vector[-2], vector[-1]),
                   radius=radius,
                   shape=shape)
     if extent is None:
@@ -21,7 +22,6 @@ def get_vdf(data,
     vdf = np.sum(data.data[xslice, yslice, rr, cc], axis=(2))
 
     if threshold is not None:
-        search = 5
         start0 = int(vector[0] - search)
         start1 = int(vector[1] - search)
         stop0 = int(vector[0] + search)
@@ -31,25 +31,29 @@ def get_vdf(data,
         if start1 < 0:
             start1 = 0
         if stop0 > vdf.shape[0]:
-            stop0 = vdf.shape[0]-1
+            stop0 = vdf.shape[0] - 1
         if stop1 > vdf.shape[1]:
-            stop1 = vdf.shape[1]-1
+            stop1 = vdf.shape[1] - 1
         sl0 = slice(start0, stop0)
         sl1 = slice(start1, stop1)
 
-        change = np.unravel_index(np.argmax(vdf[sl0, sl1]), (stop0-start0, stop1-start1))
+        change = np.unravel_index(np.argmax(vdf[sl0, sl1]),
+                                  (stop0 - start0, stop1 - start1))
         center = np.array([start0, start1]) + change
         maximum = vdf[int(center[0]), int(center[1])]
         minimum = np.min(vdf)
+
         difference = maximum - minimum
         thresh = minimum + threshold * difference
         mask = vdf > thresh
         mask = flood(mask, seed_point=(int(vector[0]), int(vector[1])))
-        if np.sum(mask) > (np.product(vdf.shape)/2):
+        if np.sum(mask) > (np.product(vdf.shape) / 2):
             vdf = np.zeros(vdf.shape)
         else:
             vdf[np.logical_not(mask)] = 0
         center = center_of_mass(vdf)
+    else:
+        center = (vector[0], vector[1])
     return center, vdf
 
 
@@ -59,12 +63,18 @@ class DiffractionVector(VectorDecomposition2D):
                     data,
                     trim_duplicates=True,
                     threshold=0.9,
+                    inplace=False,
                     **kwargs):
-        for i, v in enumerate(self.vectors):
+        if not inplace:
+            vectors = self.copy()
+        else:
+            vectors = self
+        for i, v in enumerate(vectors.vectors):
             center, vdf = get_vdf(data,
                                   v,
                                   threshold=threshold,
                                   **kwargs,)
-            self.extents.append(vdf)
-            print(self.vectors[i])
-            self.vectors[i][:2] = center
+            vectors.extents.append(vdf)
+            vectors.vectors[i][:2] = center
+        if not inplace:
+            return vectors
