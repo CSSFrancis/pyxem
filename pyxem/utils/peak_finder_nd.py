175901906aa83d4b75d2af5451861ae7a8860e78
cssfrancis <csfrancis@wisc.edu>
2022-11-29 17:30:36 -0600

    GUI: Added in additional features for peak finding GUI

--- pyxem/utils/peak_finder_nd.py
+++ pyxem/utils/peak_finder_nd.py
@@ -3,6 +3,8 @@ import traits.api as t
 from hyperspy.exceptions import SignalDimensionError
 import numpy as np
 import hyperspy.api as hs
+import matplotlib.pyplot as plt
+
 
 @add_gui_method(toolkey="pyxem.Diffraction2D.find_peaks_nd_interactive")
 class PeaksFinder2D(t.HasTraits):
@@ -11,8 +13,6 @@ class PeaksFinder2D(t.HasTraits):
     sigma2 = t.Float(1)
     sigma3 = t.Float(1)
     sigma4 = t.Float(1)
-    recalculate = t.Button()
-    #filter = t.Button()
     threshold = t.Float()
 
     show_navigation_sliders = t.Bool(False)
@@ -24,6 +24,7 @@ class PeaksFinder2D(t.HasTraits):
         self.original_signal = signal.deepcopy()
         self.signal = signal
         self.peaks = peaks
+
         if self.signal._plot is None or not self.signal._plot.is_active:
             self.signal.plot()
         if self.signal.axes_manager.navigation_size > 0:
@@ -34,22 +35,36 @@ class PeaksFinder2D(t.HasTraits):
         # Set initial parameters:
         # As a convenience, if the template argument is provided, we keep it
         # even if the method is different, to be able to use it later.
+        self.fig, self.axs = plt.subplots(1, 3, figsize=(9, 3))
+        self.axs[0].set_title("Peak Positions(Nav Axis)")
+        self.axs[1].set_title("Peak Positions (Sig Axis)")
+        self.axs[2].set_title("Peak Intensities")
+
+        self.axs[0].set_ylim(self.signal.axes_manager.navigation_extent[0:2])
+        self.axs[0].set_xlim(self.signal.axes_manager.navigation_extent[2:4])
+        self.axs[1].set_ylim(self.signal.axes_manager.signal_extent[0:2])
+        self.axs[1].set_xlim(self.signal.axes_manager.signal_extent[2:4])
 
     def filter(self):
 
         sigmas = [self.__getattribute__("sigma"+str(i))
                   for i in range(self.signal.data.ndim)]
-        print(sigmas)
+        print("filtering Data...")
         self.signal.data = -self.original_signal.filter(sigma=sigmas).data
         nav_corner = self.signal.axes_manager.navigation_extent[::2]
         sig_corner = self.signal.axes_manager.signal_extent[::2]
 
-        kernel_size = np.multiply(sigmas, 2*np.sqrt(2))
+        scales = [a.scale for a in self.signal.axes_manager._axes]
+        kernel_size = np.multiply(np.multiply(sigmas, 2*np.sqrt(2)),
+                                  scales)
+
         nav_marker = hs.plot.markers.Ellipse(x=nav_corner[0]+kernel_size[0],
-                                             y=nav_corner[1]++kernel_size[1],
+                                             y=nav_corner[1]+kernel_size[1],
                                              width=kernel_size[0],
                                              height=kernel_size[1],
-                                             fill=True, alpha=0.7)
+                                             fill=True,
+                                             alpha=0.7)
+
 
         sig_marker = hs.plot.markers.Ellipse(x=sig_corner[0]+kernel_size[2],
                                              y=sig_corner[1]+kernel_size[3],
@@ -60,10 +75,37 @@ class PeaksFinder2D(t.HasTraits):
         self.signal._plot.navigator_plot.remove_markers(render_figure=True)
         self.signal.add_marker(sig_marker)
         self.signal.add_marker(nav_marker, plot_on_signal=False)
-
         self.signal.update_plot()
-        print("filtering Data...")
 
     def find_peaks(self):
-        print(self.threshold)
-        #self.signal.find_peaks_nd()
+        self.peaks = self.signal.find_peaks_nd(threshold_abs=self.threshold, get_intensity=True)
+        if self.peaks is not None:
+            print(len(self.peaks.data), " peaks found")
+            nav_scales = [a.scale for a in self.signal.axes_manager.navigation_axes]
+            nav_offsets = [a.offset for a in self.signal.axes_manager.navigation_axes]
+            sig_scales = [a.scale for a in self.signal.axes_manager.signal_axes]
+            sig_offsets = [a.offset for a in self.signal.axes_manager.signal_axes]
+            navigation_pos = np.add(np.multiply(self.peaks.data[:, :2],
+                                                nav_scales),
+                                    nav_offsets)
+            signal_pos = np.add(np.multiply(self.peaks.data[:, 2:4],
+                                            sig_scales),
+                                sig_offsets)
+            self.axs[0].clear()
+            self.axs[1].clear()
+            self.axs[2].clear()
+            self.axs[0].set_title("Peak Positions(Nav Axis)")
+            self.axs[1].set_title("Peak Positions (Sig Axis)")
+            self.axs[2].set_title("Peak Intensities")
+            self.axs[0].scatter(navigation_pos[:, 1], navigation_pos[:, 0])
+            self.axs[1].scatter(signal_pos[:, 1], signal_pos[:, 0])
+            self.axs[0].set_xlim(self.signal.axes_manager.navigation_extent[0:2])
+            self.axs[0].set_ylim(self.signal.axes_manager.navigation_extent[2:4][::-1])
+            self.axs[1].set_xlim(self.signal.axes_manager.signal_extent[0:2])
+            self.axs[1].set_ylim(self.signal.axes_manager.signal_extent[2:4][::-1])
+            self.axs[2].hist(self.peaks.data[:,4])
+
+
+
+
+
