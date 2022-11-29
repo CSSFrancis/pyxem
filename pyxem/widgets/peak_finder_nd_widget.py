6d22728e568098f80c5a168d9850429909f6c034
cssfrancis <csfrancis@wisc.edu>
2022-11-29 17:30:36 -0600

    GUI: Added in additional features for peak finding GUI

--- pyxem/widgets/peak_finder_nd_widget.py
+++ pyxem/widgets/peak_finder_nd_widget.py
@@ -2,6 +2,7 @@ import ipywidgets
 from hyperspy_gui_ipywidgets.utils import add_display_arg
 from link_traits import link
 
+
 @add_display_arg
 def peak_finder_nd(obj):
     """ Take some signal (obj) and create a widget for finding peaks.
@@ -10,20 +11,27 @@ def peak_finder_nd(obj):
     num_dim = obj.signal.data.ndim
     sigmas = [ipywidgets.FloatText(value=1,layout=box_layout)
                               for i in range(num_dim)]
-    for i,s in enumerate(sigmas):
+    for i, s in enumerate(sigmas):
         link((obj, "sigma"+str(i)), (s, "value"))
-    sigma_box = ipywidgets.HBox([ipywidgets.Label("Filtering Sigmas: "),]+ sigmas)
+    sigma_box = ipywidgets.HBox([ipywidgets.Label("Filtering Sigmas: "), ] + sigmas)
     calculate = ipywidgets.Button(description='Find Peaks')
-    threshold = ipywidgets.FloatSlider(discription= "Threshold")
+    threshold_peaks = ipywidgets.Button(description='Threshold Peaks')
+    threshold = ipywidgets.FloatSlider(discription="Threshold")
+    link((obj, "threshold"), (threshold, "value"))
 
     filter = ipywidgets.Button(description='Filter Data',)
     buttons = ipywidgets.HBox([filter, calculate])
 
-    box = ipywidgets.VBox([sigma_box,threshold, buttons])
+    box = ipywidgets.VBox([sigma_box, threshold, buttons])
     wdict = {}
+
     def on_filter_click(b):
         obj.filter()
 
+    def on_calculate_click(b):
+        obj.find_peaks()
+
     filter.on_click(on_filter_click)
+    calculate.on_click(on_calculate_click)
     return {"widget": box, "wdict": wdict}
 
